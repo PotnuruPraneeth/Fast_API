da7945e9e39064c080e8f5ef62c2dd88fefc7935
potnuru praneeth <potnurupraneeth48@gmail.com>
2025-06-01 20:58:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,7 @@ from database_conn import Base, engine
 from movies import router_movies
 
 Base.metadata.create_all(bind=engine)
-'''SQLAlchemy:
-Looks at all classes that inherited from Base.
-Reads the __tablename__, columns, types, etc.
-Sends CREATE TABLE SQL statements to the DB.'''
 
-'''| Concept                   | Explanation                                    |
-| ------------------------- | ---------------------------------------------- |
-| `Base` knows subclasses   | ✅ Yes, **after they are defined/imported**     |
-| Happens automatically?    | ✅ Yes, **but only when you run the code**      |
-| Without running the code? | ❌ No — Python must run to register the classes |
-'''
 app = FastAPI(
     title="Movie Service",
     summary="Movie Service",
@@ -21,10 +11,4 @@ app = FastAPI(
     version="1.0.0"
 )
 
-#✅ app.include_router(router=router, prefix="/v1") — What it does:
-#This tells the main FastAPI app (app) to include all the routes from the router,
-# and add the prefix /v1 in front of them.
-'''GET /v1/hello
-→ {"message": "Hello"} #message is the function of router as i have taken from chatgpt.
-'''
-app.include_router(router=router_movies, prefix="/v1")
+router=router_movies, prefix="/v1")
